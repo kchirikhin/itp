3bbc84e5dcc7f530ef72075576bfffdddd8217fb
Konstantin Chirikhin <chirihin@gmail.com>
2019-10-24 22:13:35 +0700

    Bug fixes in MPI executor

--- itp/driver/mpi_executor.py
+++ itp/driver/mpi_executor.py
@@ -1,15 +1,18 @@
-from itp.driver.executor import executor
+from itp.driver.executor import Executor
 from mpi4py import MPI
+import math
+import itertools as it
+
 
 class MpiExecutor(Executor):
-  def __init__(base_executor, comm):
+  def __init__(self, base_executor, comm):
     self._base_executor = base_executor
     self._comm = comm
     
     
-  def execute(package):
-    size = comm.Get_size()
-    rank = comm.Get_rank()
+  def execute(self, package):
+    size = self._comm.Get_size()
+    rank = self._comm.Get_rank()
 
     chunk_size = int(math.floor(len(package) / size))
     assert(chunk_size > 0)
@@ -18,10 +21,10 @@ class MpiExecutor(Executor):
     if rank + 1 < size:
         chunk_end = chunk_size*(rank+1)
     else:
-        chunk_end = len(ts_data.columns)
+        chunk_end = len(package)
 
-    results = self._base_executor.execute(package[chunk_begin:(chunk_end-1)])
-    common_results = comm.gather(results, root=0)
+    results = self._base_executor.execute(package[chunk_begin:chunk_end])
+    common_results = self._comm.gather(results, root=0)
 
     if rank == 0:
       common_results = list(it.chain.from_iterable(common_results))
