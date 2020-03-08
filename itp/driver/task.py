5eaa9f9de8f7ae4b8ec9f8562d3f26a9ab7c155b
Konstantin Chirikhin <chirihin@gmail.com>
2020-03-08 13:12:40 +0700

    discrete univariate task were implemented

--- itp/driver/task.py
+++ itp/driver/task.py
@@ -1,10 +1,13 @@
 from abc import abstractmethod
 
+import predictor as p
+
 
 class ElementaryTask:
     """
     A task which includes only a single time series to forecast. Knows which method of predictor it should call.
     """
+
     @abstractmethod
     def run(self):
         """
@@ -24,9 +27,64 @@ class Task:
         pass
 
     @abstractmethod
-    def handle_results_of_computations(self):
+    def handle_results_of_computations(self, results):
         """
         Assembles the result of the entire task by results of execution of its elementary tasks.
+        :param results: the list of elementary results.
         :return: the assembled result.
         """
         pass
+
+
+class ItpPredictorInterface:
+    """
+    A wrapper for itp predictor functions to enable mocking
+    """
+    def forecast_multialphabet_vec(self, time_series, compressors, horizon, difference, max_quants_count, sparse):
+        return p.make_forecast_multialphabet_vec(time_series.to_list(), compressors, horizon, difference,
+                                                 max_quants_count, sparse)
+
+    def forecast_discrete(self, time_series, compressors, horizon, difference, sparse):
+        return p.make_forecast_discrete(time_series.to_list(), compressors, horizon, difference, sparse)
+
+    def forecast_multialphabet(self, time_series, compressors, horizon, difference, max_quants_count, sparse):
+        return p.make_forecast_multialphabet(time_series.to_list(), compressors, horizon, difference,
+                                             max_quants_count, sparse)
+
+
+class DiscreteUnivariateElemetaryTask(ElementaryTask):
+    """
+    Prediction of a single discrete univariate time series. Just for internal usage.
+    """
+    def __init__(self, time_series, compressors, horizon, difference, sparse,
+                 predictor_interface=ItpPredictorInterface()):
+        if predictor_interface is None:
+            raise ValueError("predictor_interface is None")
+
+        self._time_series = time_series
+        self._compressors = compressors
+        self._horizon = horizon
+        self._difference = difference
+        self._sparse = sparse
+        self._predictor_interface = predictor_interface
+
+    def run(self):
+        return self._predictor_interface.forecast_discrete(self._time_series.to_list(), self._compressors,
+                                                           self._horizon, self._difference, self._sparse)
+
+
+class DiscreteUnivariateTask(Task):
+    """
+    Prediction of a single discrete univariate time series.
+    """
+    def __init__(self, **kwargs):
+        self._elementary_task = DiscreteUnivariateElemetaryTask(**kwargs)
+
+    def get_elementary_tasks(self):
+        return self._elementary_task
+
+    def handle_results_of_computations(self, results):
+        if len(results) != 1:
+            raise ValueError("results must be a list with a single elem for DiscreteUnivariateTask")
+
+        return results[0]
---
+++ itp/tests/test_task.py
@@ -0,0 +1,27 @@
+from itp.driver.task import DiscreteUnivariateElemetaryTask, DiscreteUnivariateTask
+
+import unittest
+
+
+class TestDiscreteUnivariateElemetaryTask(unittest.TestCase):
+    def test_raises_if_passed_predictor_interface_is_none(self):
+        self.assertRaises(ValueError, DiscreteUnivariateElemetaryTask, time_series=[], compressors='',
+                          horizon=0, difference=0, sparse=-1, predictor_interface=None)
+
+
+class TestDiscreteUnivariateTask(unittest.TestCase):
+    def setUp(self):
+        self._task = DiscreteUnivariateTask(time_series=[], compressors='', horizon=0, difference=0, sparse=-1)
+
+    def test_raises_if_asked_to_handle_more_than_one_result_of_computations(self):
+        self.assertRaises(ValueError, self._task.handle_results_of_computations, [{}, {}])
+
+    def test_raises_if_asked_to_handle_empty_result(self):
+        self.assertRaises(ValueError, self._task.handle_results_of_computations, [])
+
+    def test_result_handler_returns_the_first_result(self):
+        self.assertEqual(self._task.handle_results_of_computations([1]), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
