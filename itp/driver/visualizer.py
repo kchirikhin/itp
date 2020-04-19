581cbba375e85fd3149c4056acc64f558c2d2698
Konstantin Chirikhin <chirihin@gmail.com>
2020-04-19 22:40:02 +0700

    text visualizer was implemented

--- itp/driver/visualizer.py
+++ itp/driver/visualizer.py
@@ -24,6 +24,21 @@ class TextVisualizer(Visualizer):
     def visualize(self, history, forecasting_result):
         if self._description is not None:
             print(self._description)
-        for value in forecasting_result[self._compressor].series(self._series_number):
-            print(value, end=' ')
+        print(self._compressor + ':')
+
+        self._print_row(forecasting_result.forecast, 'forecast')
+
+        if forecasting_result.has_relative_errors(self._compressor):
+            self._print_row(forecasting_result.relative_errors, 'relative_errors')
+
+        if forecasting_result.has_lower_bounds(self._compressor):
+            self._print_row(forecasting_result.lower_bounds, 'lower_bounds')
+
+        if forecasting_result.has_upper_bounds(self._compressor):
+            self._print_row(forecasting_result.upper_bounds, 'upper_bounds')
+
+    def _print_row(self, getter, title):
+        print(title, end=':')
+        for value in getter(self._compressor).series(self._series_number):
+            print(' ' + str(value), end='')
         print('')
