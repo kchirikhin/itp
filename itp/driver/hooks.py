e9386305cad213bb5a00116019dc70c9f2328a60
Konstantin Chirikhin <chirihin@gmail.com>
2020-06-07 16:58:33 +0700

    Lag differencing and combined hooks were implemented, smoothing hook was fixed

--- itp/driver/hooks.py
+++ itp/driver/hooks.py
@@ -45,7 +45,7 @@ class EmptyHook(IHook):
     def preprocess(self, time_series: TimeSeries):
         return time_series
 
-    def postprocess(self, result: Dict[str, Union[List[Union[int, float]], List[List[Union[int, float]]]]]):
+    def postprocess(self, result: Dict[str, TimeSeries]):
         return result
 
 
@@ -60,11 +60,66 @@ class BasicSmoothingHook(IHook):
 
         new_time_series = time_series[:(len(time_series) - 2)]
         for i in range(2, len(time_series)):
-            time_series[i-2] = (2 * time_series[i] + time_series[i-1] + time_series[i-2]) / 4
+            new_time_series[i-2] = (2 * time_series[i] + time_series[i-1] + time_series[i-2]) / 4
 
         return new_time_series
 
-    def postprocess(self, result: Dict[str, Union[List[Union[int, float]], List[List[Union[int, float]]]]]):
+    def postprocess(self, result: Dict[str, TimeSeries]):
         return result
 
     _minimal_ts_len = 3
+
+
+class LagDifferencingHook(IHook):
+    """
+    A hook which transforms the series by taking the differences between values of the original series with specified
+    lag.
+    """
+    def __init__(self, lag: int = 1):
+        self._time_series = None
+        self._lag = lag
+
+    def preprocess(self, time_series: TimeSeries):
+        if len(time_series) <= self._lag:
+            return time_series
+
+        self._time_series = time_series
+        new_time_series = time_series[self._lag:]
+        for i in range(len(new_time_series)):
+            new_time_series[i] = time_series[i + self._lag] - time_series[i]
+
+        return new_time_series
+
+    def postprocess(self, result: Dict[str, TimeSeries]):
+        if self._time_series is None:
+            return result
+
+        ts_len = len(self._time_series)
+        for compressor, forecast in result.items():
+            for i in range(len(forecast)):
+                if ts_len + i - self._lag < len(self._time_series):
+                    forecast[i] = self._time_series[ts_len + i - self._lag] + forecast[i]
+                else:
+                    forecast[i] = forecast[i - self._lag] + forecast[i]
+
+        return result
+
+
+class CombinedHook(IHook):
+    """
+    A hook which calls other hooks in sequence.
+    """
+    def __init__(self, *args):
+        self._hooks = list(args)
+
+    def preprocess(self, time_series: TimeSeries):
+        for hook in self._hooks:
+            time_series = hook.preprocess(time_series)
+
+        return time_series
+
+    def postprocess(self, result: Dict[str, TimeSeries]):
+        for hook in reversed(self._hooks):
+            result = hook.postprocess(result)
+
+        return result
---
+++ itp/tests/test_hooks.py
@@ -0,0 +1,66 @@
+from itp.driver.hooks import LagDifferencingHook, CombinedHook, BasicSmoothingHook, HookError
+from itp.driver.time_series import TimeSeries
+import unittest
+
+
+class TestBasicSmoothingHook(unittest.TestCase):
+    def setUp(self):
+        self._hook = BasicSmoothingHook()
+
+    def test_signals_if_length_is_not_enough(self):
+        series = TimeSeries([1], dtype=int, frequency=1)
+        self.assertRaises(HookError, self._hook.preprocess, series)
+
+    def test_works_correctly_on_series_of_minimal_correct_length(self):
+        series = TimeSeries([1, 2, 3], dtype=int, frequency=1)
+        expected_result = TimeSeries([(2*3 + 2 + 1)/4], dtype=int, frequency=1)
+        self.assertEqual(self._hook.preprocess(series), expected_result)
+
+
+class TestLagDifferencingHook(unittest.TestCase):
+    def setUp(self) -> None:
+        self._hook = LagDifferencingHook(5)
+
+    def test_returns_input_series_if_its_size_less_than_lag(self):
+        series = TimeSeries([1, 2, 3], dtype=int, frequency=1)
+        self.assertEqual(self._hook.preprocess(series), series)
+
+    def test_returns_input_series_if_lag_equals_to_series_length(self):
+        series = TimeSeries([0, 1, 2, 3, 4], dtype=int, frequency=1)
+        self.assertEqual(self._hook.preprocess(series), series)
+
+    def test_works_correctly_if_series_length_greater_than_lag(self):
+        series = TimeSeries([0, 1, 2, 3, 4, 5, 7, 9, 11], dtype=int, frequency=1)
+        expected_series = TimeSeries([5, 6, 7, 8], dtype=int, frequency=1)
+        self.assertEqual(self._hook.preprocess(series), expected_series)
+
+    def test_reverse_transformation_works_correctly(self):
+        series = TimeSeries([0, 1, 2, 3, 4, 5, 7, 9, 11], dtype=int, frequency=1)
+        result = {'zlib': TimeSeries([8, 6, 6], dtype=int, frequency=1),
+                  'ppmd': TimeSeries([8, 7, 6], dtype=int, frequency=1)}
+        expected_result = {'zlib': TimeSeries([12, 11, 13], dtype=int, frequency=1),
+                           'ppmd': TimeSeries([12, 12, 13], dtype=int, frequency=1)}
+
+        self._hook.preprocess(series)
+        self.assertEqual(self._hook.postprocess(result), expected_result)
+
+
+class TestCombinedHook(unittest.TestCase):
+    def setUp(self) -> None:
+        self._series = TimeSeries([1, 2, 3, 4, 5, 3, 4, 5, 5, 6, 7, 8, 9, 10], dtype=int, frequency=1)
+        self._hook = CombinedHook(BasicSmoothingHook(), LagDifferencingHook(3))
+
+    def test_preprocessing_made_correctly(self):
+        actual_result = self._hook.preprocess(self._series)
+        expected_result = TimeSeries([1, 1, 0, 1, 1, 2, 3, 3, 3], dtype=int, frequency=1)
+        self.assertEqual(actual_result, expected_result)
+
+    def test_postprocessing_made_correctly(self):
+        forecasts = {'zlib': TimeSeries([2, 3, 2, 3], dtype=int, frequency=1),
+                     'ppmd': TimeSeries([3, 3, 2, 3], dtype=int, frequency=1)}
+        expected_result = {'zlib': TimeSeries([9, 11, 11, 12], dtype=int, frequency=1),
+                           'ppmd': TimeSeries([10, 11, 11, 13], dtype=int, frequency=1)}
+
+        self._hook.preprocess(self._series)
+        actual_result = self._hook.postprocess(forecasts)
+        self.assertEqual(actual_result, expected_result)
