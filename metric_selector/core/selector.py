5d40010e0d42755eeb66017fa73e71c874961fa8
kchirikhin <chirihin@gmail.com>
2020-11-12 17:14:53 +0700

    Template for selector

---
+++ metric_selector/core/selector.py
@@ -0,0 +1,10 @@
+"""
+An algorithm that selects the best data compressor predicting already known values.
+"""
+
+from algorithm import Algorithm
+from metric import Metric
+
+
+def select(sample_generator, algorithms, metric, h):
+    pass
